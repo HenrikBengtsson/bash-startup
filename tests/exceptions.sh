65dc5ae96854ac0d39f480d4eabdbddad8b2775e
hb <hb@aroma-project.org>
2017-12-11 16:24:41 -0800

    TESTS: Adding tests for exceptions/errors

---
+++ tests/exceptions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## Calling as executable instead of source:ing
+
+echo "./bash-startup ..."
+
+./bash-startup
+value=$?
+echo "Exit code: $value"
+[[ $value -eq 0 ]] && exit 1
+
+res=$( { ./bash-startup ; } 2>&1 )
+value=$?
+echo "Results: $res"
+echo "Exit code: $value"
+[[ $value -eq 0 ]] && exit 1
+
+echo "./bash-startup ... done"
