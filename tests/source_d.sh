df4aca144b6d16c43982664a544d92fc32158524
hb <hb@aroma-project.org>
2017-12-11 18:47:43 -0800

    TESTS: Testing source_d

--- tests/source_d.sh
+++ tests/source_d.sh
@@ -16,6 +16,21 @@ source_d examples/.bashrc.d/
 export STARTUP_DEBUG=0
 source_d examples/.bashrc.d/ examples/.bashrc.d/
 
+## Assert that dryrun does not output anything to standard output
+export STARTUP_DRYRUN=true
+res=$(source_d examples/.bashrc.d/)
+[[ -z $res ]] && exit 1
+
+export STARTUP_DEBUG=true
+res=$(source_d examples/.bashrc.d/)
+[[ -z $res ]] && exit 1
+
+## Assert that non-dryrun outputs something to standard output
+export STARTUP_DRYRUN=
+export STARTUP_DEBUG=
+res=$(source_d examples/.bashrc.d/)
+[[ -n $res ]] && exit 1
+
 echo "source_d ... done"
 
 echo "TEST STATUS: OK"
