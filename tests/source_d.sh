c10118880bced56631708c015bba46b35f7083b5
hb <hb@aroma-project.org>
2017-12-11 18:47:32 -0800

    TESTS: Testing source_d

--- tests/source_d.sh
+++ tests/source_d.sh
@@ -13,6 +13,9 @@ source_d examples/.bashrc.d/
 export STARTUP_DEBUG=2
 source_d examples/.bashrc.d/
 
+export STARTUP_DEBUG=0
+source_d examples/.bashrc.d/ examples/.bashrc.d/
+
 echo "source_d ... done"
 
 echo "TEST STATUS: OK"
